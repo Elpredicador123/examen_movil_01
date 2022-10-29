2faf3ee696501ef9a05e393e96a6cdf7bc8bba05
Elpredicador123 <agentepredicador@gmail.com>
2022-10-29 15:45:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app_movil',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
